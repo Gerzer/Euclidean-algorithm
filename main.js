cb30deb9159d2cbfdbd34a9dcd87464c97e13ac7
Gerzer <gjacobycooper_16@speyerlegacyschool.org>
2015-06-09 12:11:05 -0400

    Add scroll support

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ function updateTimer() {
 function startTimer() {
   timer = window.setInterval(updateTimer, 1000);
 }
+function isTouchDevice() {
+  return !!('ontouchstart' in window);
+}
+function initializeSkrollr() {
+  if (!isTouchDevice()) {
+    var containers = document.getElementsByClassName("container");
+    for (var i = 0; i < containers.length; i++) {
+      containers[i].setAttribute("data--45p-top-top", "-webkit-transform: scale(1, 1) rotate3d(0, 0, 0, 0deg) translate(0%, 0%); opacity: 1.0;");
+      containers[i].setAttribute("data-0-top-bottom", "-webkit-transform: scale(0, 0) rotate3d(135, 225, 225, 135deg) translate(0%, 0%); opacity: 1.0;");
+      containers[i].setAttribute("data-0-bottom-top", "-webkit-transform: scale(1, 1) rotate3d(0, 0, 0, 0deg) translate(-17%, -10%); opacity: 0.3;");
+      containers[i].setAttribute("data-0-bottom-bottom", "-webkit-transform: scale(1, 1) rotate3d(0, 0, 0, 0deg) translate(0%, 0%); opacity: 1.0;");
+    }
+    skrollr.init();
+  }
+}
