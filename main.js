79b8fbfb426ab3618028faa118db396b29becca9
Gerzer <gjacobycooper_16@speyerlegacyschool.org>
2015-06-10 11:45:17 -0400

    Remove logging

--- main.js
+++ main.js
@@ -47,9 +47,6 @@ function initializeSkrollr() {
     randomIntegerB = getRandomInteger(150, 5000);
     GCDResult = getGCD(randomIntegerA, randomIntegerB);
   }
-  console.log(getRandomInteger(150, 5000).toString());
-  console.log(randomIntegerA.toString());
-  console.log(randomIntegerB.toString());
   document.getElementById("integer-a").textContent = randomIntegerA.toString();
   document.getElementById("integer-b").textContent = randomIntegerB.toString();
 }
